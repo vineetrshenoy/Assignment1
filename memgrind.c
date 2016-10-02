5e30846132e5b7fcfc877f261b678aa092df51ab
vineetrshenoy <vineetrshenoy@users.noreply.github.com>
2016-10-01 20:27:32 -0400

    Added Files to remote repository

---
+++ memgrind.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "mymalloc.h"
+
+void memvoid(){
+	printf("Entered the memvoid method\n");
+}
---
+++ mymalloc.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "mymalloc.h"
+
+void test(){
+	printf("Entered the void method\n");
+	memvoid();
+}
+
+
+int main(){
+
+
+	test();
+	printf("Done\n");
+}
