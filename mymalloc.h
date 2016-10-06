773277c69af69b44924106e2dd9c065b34c455f1
vineetrshenoy <vineetrshenoy@users.noreply.github.com>
2016-10-06 17:14:57 -0400

    Project cleanup

--- mymalloc.h
+++ mymalloc.h
@@ -1,2 +1,7 @@
 void test();
 void memvoid();
+char * getHeader(char * p);
+char * getFooter(char * p);
+int getSize(char * headerPointer);
+int getAllocation(char * p);
+char * createExtremities(char * p, int size, int allocated);
